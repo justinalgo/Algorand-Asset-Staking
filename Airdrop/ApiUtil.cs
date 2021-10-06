7884f7c8b1b4b56764c950229c59153f5149f929
Wesley Miller <millerwes1998@gmail.com>
2021-10-06 11:42:47 -0400

    Add project files.

---
+++ Airdrop/ApiUtil.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Web;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using Algorand.V2;
+using Algorand.V2.Model;
+using System.Threading;
+
+namespace Airdrop
+{
+    class ApiUtil
+    {
+        private readonly ILogger<ApiUtil> _log;
+        private readonly AlgodApi _algod;
+        private readonly IndexerApi _indexer;
+
+        public ApiUtil(ILogger<ApiUtil> log, AlgodApi algod, IndexerApi indexer)
+        {
+            this._log = log;
+            this._algod = algod;
+            this._indexer = indexer;
+        }
+        public ApiUtil(AlgodApi algod, IndexerApi indexer)
+        {
+            this._algod = algod;
+            this._indexer = indexer;
+        }
+
+        public List<string> GetWalletAddressesWithAsset(int assetId)
+        {
+            var assetInfo = _indexer.LookupAssetBalances(assetId);
+
+            List<string> walletAddresses = new List<string>();
+
+            foreach (MiniAssetHolding account in assetInfo.Balances)
+            {
+                walletAddresses.Add(account.Address);
+            }
+
+            while (assetInfo.NextToken != null)
+            {
+                Thread.Sleep(1200); //PureStake sleep
+                assetInfo = _indexer.LookupAssetBalances(assetId, next: assetInfo.NextToken);
+
+                foreach (MiniAssetHolding account in assetInfo.Balances)
+                {
+                    walletAddresses.Add(account.Address);
+                }
+            }
+
+            return walletAddresses;
+        }
+
+        public IEnumerable<string> GetWalletAddressesWithAsset(int assetId, params int[] assetIds)
+        {
+            IEnumerable<string> addresses = GetWalletAddressesWithAsset(assetId);
+
+            foreach (int id in assetIds)
+            {
+                IEnumerable<string> intersectAddresses = GetWalletAddressesWithAsset(id);
+                addresses = addresses.Intersect(intersectAddresses);
+            }
+
+            return addresses;
+        }
+
+        public long GetAssetDecimals(int assetId)
+        {
+            return _algod.GetAssetByID(assetId).Params.Decimals.Value;
+        }
+
+        public Account GetAccountByAddress(string walletAddress)
+        {
+            return _algod.AccountInformation(walletAddress);
+        }
+    }
+}
---
+++ Airdrop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Algorand;
+using Algorand.V2;
+using Algorand.Client;
+using Algorand.V2.Model;
+using Account = Algorand.Account;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Airdrop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ALGOD_API_ADDR = "http://localhost:8111";
+            string ALGOD_API_TOKEN = "4430a9d11f819af4ed7bc1cbba3545d06230ed9499918902c40d4caaea7d4540";
+
+            string INDEXER_API_ADDR = "https://mainnet-algorand.api.purestake.io/idx2";
+            string INDEXER_API_TOKEN = "HFoxXc2sQf7ut4bAVmfg0adKQ6RRqTCi6nEg0YIs";
+
+            AlgodApi algod = new AlgodApi(ALGOD_API_ADDR, ALGOD_API_TOKEN);
+            IndexerApi indexer = new IndexerApi(INDEXER_API_ADDR, INDEXER_API_TOKEN);
+            ApiUtil api = new ApiUtil(algod, indexer);
+            
+            //IEnumerable<string> dropAddresses = api.GetWalletAddressesWithAsset(310014962, 320570576);
+
+            List<AssetValue> alchemonValues = JsonConvert.DeserializeObject<List<AssetValue>>(File.ReadAllText("C:/Users/ParkG/source/repos/Airdrop/Airdrop/AlchemonValues.json"));
+
+            Dictionary<long, AssetValue> assetValues = alchemonValues.ToDictionary(av => av.Id);
+
+
+
+            Console.WriteLine(val);
+        }
+    }
+
+    public class AssetValue
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("value")]
+        public long Value { get; set; }
+
+        [JsonProperty("decimals")]
+        public long Decimals { get; set; }
+    }
+}
