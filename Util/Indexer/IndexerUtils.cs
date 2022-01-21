c2e4853f7fde6f275d6ca41d418559b03a9f8704
Wes <ParkGalaxy@live.com>
2022-01-21 16:56:27 -0500

    Temporary changes before Algorand sdk is updated

--- Util/Indexer/IndexerUtils.cs
+++ Util/Indexer/IndexerUtils.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -57,6 +58,7 @@ namespace Utils.Indexer
                                          auth_addr: null,
                                          round: null,
                                          application_id: null);
+
                 foreach (Account account in response.Accounts)
                 {
                     walletAddresses.Add(account.Address);
@@ -66,5 +68,137 @@ namespace Utils.Indexer
             return walletAddresses;
         }
 
+        public async Task<IEnumerable<string>> GetWalletAddresses(int assetId, params int[] assetIds)
+        {
+            IEnumerable<string> addresses = await GetWalletAddresses(assetId);
+
+            foreach (int id in assetIds)
+            {
+                IEnumerable<string> intersectAddresses = await GetWalletAddresses(id);
+                addresses = addresses.Intersect(intersectAddresses);
+            }
+
+            return addresses;
+        }
+
+        public async Task<IEnumerable<string>> GetWalletAddresses(string address, AddressRole addressRole, int assetId, TxType txType, ulong? currencyGreaterThan = null, ulong? currencyLessThan = null, ulong? minRound = null)
+        {
+            List<string> walletAddresses = new List<string>();
+
+            IEnumerable<Transaction> transactions = await GetTransactions(address, addressRole, assetId, txType, currencyGreaterThan, currencyLessThan, minRound);
+
+            Func<Transaction, string> getAddress;
+
+            if (addressRole == AddressRole.Sender)
+            {
+                getAddress = (Transaction txn) => txn.Sender;
+            }
+            else
+            {
+                switch(txType)
+                {
+                    case TxType.Axfer:
+                        getAddress = (Transaction txn) => txn.AssetTransferTransaction.Receiver;
+                        break;
+                    case TxType.Pay:
+                        getAddress = (Transaction txn) => txn.PaymentTransaction.Receiver;
+                        break;
+                    case TxType.Afrz:
+                        getAddress = (Transaction txn) => txn.AssetFreezeTransaction.Address;
+                        break;
+                    default:
+                        getAddress = (Transaction txn) => null;
+                        break;
+                }
+            }
+
+            foreach (Transaction transaction in transactions)
+            {
+                string addr = getAddress(transaction);
+                if (addr != null)
+                {
+                    walletAddresses.Add(addr);
+                }
+            }
+
+            return walletAddresses;
+        }
+        
+        public async Task<Asset> GetAsset(ulong assetId)
+        {
+            Response9 response = await lookupApi.AssetsAsync(assetId, null);
+
+            return response.Asset;
+        }
+
+        public async Task<IEnumerable<Asset>> GetAsset(IEnumerable<int> assetIds)
+        {
+            List<Asset> assets = new List<Asset>();
+
+            foreach (int assetId in assetIds)
+            {
+                assets.Add(await GetAsset(assetId));
+            }
+
+            return assets;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetTransactions(string address, AddressRole addressRole, int assetId, TxType txType, ulong? currencyGreaterThan = null, ulong? currencyLessThan = null, ulong? minRound = null)
+        {
+            List<Transaction> transactions = new List<Transaction>();
+
+            Response4 response = await searchApi.TransactionsAsync(
+                    limit: 100,
+                    next: null,
+                    note_prefix: null,
+                    tx_type: txType,
+                    sig_type: null,
+                    txid: null,
+                    round: null,
+                    min_round: minRound,
+                    max_round: null,
+                    asset_id: assetId,
+                    before_time: null,
+                    after_time: null,
+                    currency_greater_than: currencyGreaterThan,
+                    currency_less_than: currencyLessThan,
+                    address: address,
+                    address_role: addressRole,
+                    exclude_close_to: null,
+                    rekey_to: null,
+                    application_id: null
+                );
+
+            transactions.AddRange(response.Transactions);
+
+            while (response.NextToken != null)
+            {
+                response = await searchApi.TransactionsAsync(
+                    limit: 100,
+                    next: response.NextToken,
+                    note_prefix: null,
+                    tx_type: null,
+                    sig_type: null,
+                    txid: null,
+                    round: null,
+                    min_round: minRound,
+                    max_round: null,
+                    asset_id: assetId,
+                    before_time: null,
+                    after_time: null,
+                    currency_greater_than: currencyGreaterThan,
+                    currency_less_than: currencyLessThan,
+                    address: address,
+                    address_role: addressRole,
+                    exclude_close_to: null,
+                    rekey_to: null,
+                    application_id: null
+                );
+
+                transactions.AddRange(response.Transactions);
+            }
+
+            return transactions;
+        }
     }
 }
