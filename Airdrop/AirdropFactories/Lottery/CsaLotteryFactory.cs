18a14e3830aa898a5a7669a1e727cbab5c3405e1
Wes <ParkGalaxy@live.com>
2022-02-01 19:09:24 -0500

    Initial Lottery Implementation

---
+++ Airdrop/AirdropFactories/Lottery/CsaLotteryFactory.cs
@@ -0,0 +1,69 @@
+﻿using Algorand.V2.Indexer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utils.Indexer;
+
+namespace Airdrop.AirdropFactories.Lottery
+{
+    public class CsaLotteryFactory : LotteryAirdropFactory
+    {
+        private readonly IIndexerUtils indexerUtils;
+        private readonly string[] CreatorAddresses;
+
+        public CsaLotteryFactory(IIndexerUtils indexerUtils)
+        {
+            this.AssetId = 226265212;
+            this.Decimals = 0;
+            this.CreatorAddresses = new string[] { "ACORNOX3WJKR2GX63IJGPC2MV6E7GRNBBEE22BXQ6H7HFMEXLUL3RSEBLY" };
+            this.NumberOfWinners = 10;
+            this.TotalWinnings = 221312;
+            this.indexerUtils = indexerUtils;
+        }
+
+        public override async Task<IEnumerable<AirdropAmount>> FetchAirdropAmounts()
+        {
+            HashSet<ulong> assetIds = await this.FetchAssetIds();
+            IEnumerable<Account> accounts = await this.FetchAccounts();
+            List<(Account, ulong)> eligibleWinners = new List<(Account, ulong)>();
+            List<AirdropAmount> winners = new List<AirdropAmount>();
+
+            foreach (Account account in accounts) {
+                eligibleWinners.AddRange(this.GetEligibleWinners(account, assetIds));
+            }
+
+            Random random = new Random();
+            ulong i = 0;
+
+            while (i < this.NumberOfWinners && eligibleWinners.Count > 0)
+            {
+                int winnerIndex = random.Next(eligibleWinners.Count);
+
+                (Account, ulong) winner = eligibleWinners.ElementAt(winnerIndex);
+                eligibleWinners.RemoveAt(winnerIndex);
+
+                winners.Add(new AirdropAmount(winner.Item1.Address, this.AssetId, this.TotalWinnings / this.NumberOfWinners));
+
+                i++;
+            }
+
+            return winners;
+        }
+
+        public override async Task<HashSet<ulong>> FetchAssetIds()
+        {
+            Account account = await this.indexerUtils.GetAccount(this.CreatorAddresses[0]);
+
+            return account.CreatedAssets.Select(ca => ca.Index).ToHashSet();
+        }
+
+        public override async Task<IEnumerable<Account>> FetchAccounts()
+        {
+            IEnumerable<Account> accounts = await this.indexerUtils.GetAccounts(this.AssetId);
+
+            return accounts;
+        }
+    }
+}
---
+++ Airdrop/AirdropFactories/Lottery/LotteryAirdropFactory.cs
@@ -0,0 +1,39 @@
+﻿using Algorand.V2.Indexer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airdrop.AirdropFactories.Lottery
+{
+    public abstract class LotteryAirdropFactory : ILotteryAirdropFactory
+    {
+        public ulong NumberOfWinners { get; set; }
+        public ulong AssetId { get; set; }
+        public ulong Decimals { get; set; }
+        public ulong TotalWinnings { get; set; }
+        public abstract Task<IEnumerable<AirdropAmount>> FetchAirdropAmounts();
+
+        public abstract Task<HashSet<ulong>> FetchAssetIds();
+
+        public abstract Task<IEnumerable<Account>> FetchAccounts();
+
+        public IEnumerable<(Account, ulong)> GetEligibleWinners(Account account, HashSet<ulong> assetIds)
+        {
+            List<(Account, ulong)> eligibleAssets = new List<(Account, ulong)>();
+
+            if (account.Assets != null)
+            {
+                foreach (AssetHolding assetHolding in account.Assets)
+                {
+                    if (assetIds.Contains(assetHolding.AssetId))
+                    {
+                        eligibleAssets.Add((account, assetHolding.AssetId));
+                    }
+                }
+            }
+
+            return eligibleAssets;
+        }
+    }
+}
