9ab862ac6eb383eea3eac445f1e96d053f369039
Wesley Miller <millerwes1998@gmail.com>
2022-04-25 19:21:26 -0400

    Fix Mantis decimal places

--- Airdrop/AirdropFactories/Holdings/MantisHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/MantisHoldingsFactory.cs
@@ -14,7 +14,7 @@ namespace Airdrop.AirdropFactories.Holdings
         public MantisHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils) : base(indexerUtils, algodUtils)
         {
             this.DropAssetId = 714976897;
-            this.Decimals = 0;
+            this.Decimals = 8;
             this.CreatorAddresses = new string[] {
                 "AII4BCB5GN6YMSP7TNWMQRM4X2SVZY7AUMKQAGLMZBKZFC2LUWAER2CVOQ",
             };
