1ac14d319b03600896a08a1550ac23e126cf20f6
Wesley Miller <millerwes1998@gmail.com>
2022-05-08 23:27:33 -0400

    Add highhogs

---
+++ Airdrop/AirdropFactories/Holdings/HighHogHoldingsFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Utils.Algod;
+using Utils.Indexer;
+
+namespace Airdrop.AirdropFactories.Holdings
+{
+    public class HighHogHoldingsFactory : RandHoldingsAirdropFactory
+    {
+        public HighHogHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils, IHttpClientFactory httpClientFactory) : base(indexerUtils, algodUtils, httpClientFactory.CreateClient())
+        {
+            this.DropAssetId = 455356741;
+            this.Decimals = 0;
+            this.CreatorAddresses = new string[] { "YZEDY575NGDQPBQT3FD5C6S56DFNNICIGN32X2GI6AC63MVOPZMWVOZY7Y" };
+            this.AssetValue = 5000;
+        }
+    }
+}
