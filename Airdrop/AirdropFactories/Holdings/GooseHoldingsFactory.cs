b53f00286823d1f63f28a6ff0439a1eace6258b6
Wesley Miller <millerwes1998@gmail.com>
2022-05-30 14:14:15 -0400

    Add Goose Factory

---
+++ Airdrop/AirdropFactories/Holdings/GooseHoldingsFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utils.Algod;
+using Utils.Indexer;
+
+namespace Airdrop.AirdropFactories.Holdings
+{
+    public class GooseHoldingsFactory : HoldingsAirdropFactory
+    {
+        public GooseHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils) : base(indexerUtils, algodUtils)
+        {
+            this.DropAssetId = 751294723;
+            this.Decimals = 0;
+            this.CreatorAddresses = new string[] { "GOOSE4NW53JZLCG6NX37WWPUAOOYSAHPVPPEMBI7FGZZBZ4OET27JV4O3U",
+                "GOOSECHXVEKJ4SO43NTW5HXOIGLFGC2SQDAVWQGJCN576ODJ5SECV6MUOM",
+                "GOOSE7PN4S366W5LLQ3TRO4BCB2C66VBSMMXRVAWAPGZJHJYR34VNK2AU4",
+                "GOOSEOQSO2XM54KNCN2ESH3A7VCHRFSCFMACE24QLBUGCR256JOGRCYSSI",
+                "GOOSEKPIKOZPEPBMFO7TRRR2EPXLWKOIBLKXJKXWMK2J56SOXWRC3FLNSU" };
+            this.AssetValue = 6;
+        }
+    }
+}
