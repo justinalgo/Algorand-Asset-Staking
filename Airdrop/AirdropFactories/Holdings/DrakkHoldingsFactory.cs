fc105120cc95419676263adcd13fc43aebf77508
Wesley Miller <millerwes1998@gmail.com>
2022-06-18 19:16:13 -0400

    Update DrakkFactory

--- Airdrop/AirdropFactories/Holdings/DrakkHoldingsFactory.cs
+++ Airdrop/AirdropFactories/Holdings/DrakkHoldingsFactory.cs
@@ -14,16 +14,14 @@ namespace Airdrop.AirdropFactories.Holdings
     public class DrakkHoldingsFactory : RandHoldingsAirdropFactory
     {
         private readonly ICosmos cosmos;
-        private readonly ulong liquidityAssetId;
 
         public DrakkHoldingsFactory(IIndexerUtils indexerUtils, IAlgodUtils algodUtils, ICosmos cosmos, IHttpClientFactory httpClientFactory) : base(indexerUtils, algodUtils, httpClientFactory.CreateClient())
         {
             this.DropAssetId = 560039769;
             this.Decimals = 6;
             this.CreatorAddresses = new string[] { "UXXYI4CPUIZ27UTWNL42VO7EG5LQGRQHLNKD2JIPVHEBZ7T7JXYOHAGW4A" };
-            this.RevokedAddresses = new string[] { "7VGVH2G7R7MM7HNRV6AFIC7HP3WXIK6CZ42GGSK6LRRMREGNOQXRATBMLA", "UXXYI4CPUIZ27UTWNL42VO7EG5LQGRQHLNKD2JIPVHEBZ7T7JXYOHAGW4A" };
+            this.RevokedAddresses = new string[] { "7VGVH2G7R7MM7HNRV6AFIC7HP3WXIK6CZ42GGSK6LRRMREGNOQXRATBMLA", "UXXYI4CPUIZ27UTWNL42VO7EG5LQGRQHLNKD2JIPVHEBZ7T7JXYOHAGW4A", "B6PH4HYVR6MJOOQMFFJ3VFNHJO3NTBGHQBCFOHT4KKI6CGWI3DW2AZBMNM", "5MJJG6FPU43HRHGOMHWZT5ZGNCUD7DHICWF45GJVIEZDRQKS3M7KWMJWAQ" };
             this.cosmos = cosmos;
-            this.liquidityAssetId = 572918686;
         }
 
         public override async Task<IDictionary<ulong, ulong>> FetchAssetValues()
@@ -31,7 +29,9 @@ namespace Airdrop.AirdropFactories.Holdings
             IEnumerable<AssetValue> assets = await cosmos.GetAssetValues("Drakk");
 
             Dictionary<ulong, ulong> assetValues = assets.ToDictionary(av => av.AssetId, av => (ulong) (av.Value * Math.Pow(10, this.Decimals)));
-            assetValues[this.liquidityAssetId] = 2;
+            assetValues[572918686] = 2;
+            assetValues[776390976] = 2;
+            assetValues[776405295] = 2;
 
             return assetValues;
         }
